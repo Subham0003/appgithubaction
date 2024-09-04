0c1614e3d627e0d837915d43d584f605ae0bee63
Subham0003 <subham0003@gmail.com>
2024-09-04 21:27:41 +0530

    correction

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -11,7 +11,6 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(-1, 2) == -3
     assert subtract(0, -5) == 5
-    assert subtract(2, -1) ==1
-    assert subtract(0,0)==0
+ 
     
     print("All tests passed!")
